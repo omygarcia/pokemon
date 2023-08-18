3c8daed657ced39d56afc7f6020dc7200e59bd36
Rafael <rpalafox@sinprol.com.mx>
2023-08-18 13:22:27 -0600

    primer commit

---
+++ src/helpers/getPokemonOptions.js
@@ -0,0 +1,45 @@
+import pokemonApi from "@/api/pokemonApi";
+
+const getPokemons = ()=>{
+    const pokemonArr = Array.from(Array(650));
+    //console.log(pokemonArr);
+    return pokemonArr.map((_,index)=>index+1);
+}
+
+
+const getPokemonsOptions = async()=>{
+    const mixedPokemons = getPokemons().sort(()=>Math.random()-0.5);
+    //console.log(getPokemons());
+    //console.log(mixedPokemons.splice(0,4));
+    const pokemons = await getPokemonsNames(mixedPokemons.splice(0,4));
+    //console.table(pokemons);
+    return pokemons;
+}
+
+const getPokemonsNames = async([a,b,c,d] = [])=>{
+    //const resp = await pokemonApi.get('/1')
+    //console.log(a,b,c,d,resp.data.name,resp.data.id);
+    //console.log('abcd',a,b,c,d);
+
+    const promiseArr = [
+        pokemonApi.get(`/${a}`),
+        pokemonApi.get(`/${b}`),
+        pokemonApi.get(`/${c}`),
+        pokemonApi.get(`/${d}`),
+    ];
+
+    const [p1,p2,p3,p4] = await Promise.all(promiseArr);
+
+    //console.log(resp);
+    console.log(p1);
+
+    return [
+        {name:p1.data.name,id:p1.data.id},
+        {name:p2.data.name,id:p2.data.id},
+        {name:p3.data.name,id:p3.data.id},
+        {name:p4.data.name,id:p4.data.id},
+    ]
+}
+
+
+export default getPokemonsOptions;
---
+++ src/pages/PokemonPage.vue
@@ -0,0 +1,75 @@
+<template>
+    <h1 v-if="!pokemon">Espere por favor...</h1>
+    <div v-else>
+        <h4>¿Quién es este pokémon?</h4>
+        <PokemonPicture :pokemonId="pokemon.id" :showPokemon="showPokemon" />
+        <PokemonOptions :pokemons="pokemonArr" @selection-pokemon="checkAnswer" />
+        <template v-if="showAnswer">
+            <h2 class="fade-in">{{message}}</h2>
+            <button @click="newGame">Juego nuevo</button>
+        </template>
+    </div>
+</template>
+
+<script>
+    import PokemonOptions from '@/components/PokemonOptions';
+    import PokemonPicture from '@/components/PokemonPicture';
+    import getPokemonOptions from '@/helpers/getPokemonOptions';
+
+    //console.log(getPokemonOptions());
+
+    export default{
+        components:{
+            PokemonOptions,
+            PokemonPicture
+        },
+        data(){
+            return{
+                pokemonArr:[],
+                pokemon:null,
+                showPokemon:false,
+                showAnswer:false,
+                message:""
+            }
+        },
+        methods:{
+            async mixPokemonArry(){
+                this.pokemonArr = await getPokemonOptions();
+                //console.log(this.pokemonArr);
+                const rndInt = Math.floor(Math.random()*4);
+                //console.log('rndInt',rndInt);
+                this.pokemon = this.pokemonArr[rndInt];
+                console.log(this.pokemon);
+            },
+            checkAnswer(selectedId){
+                console.log('pokemon llamado desde evento',selectedId);
+                this.showPokemon = true;
+                this.showAnswer = true;
+
+                if(selectedId == this.pokemon.id)
+                {
+                    this.message = `Correcto, ${this.pokemon.name}`;
+                }else{
+                    this.message = `Oops, era ${this.pokemon.name}`;
+                }
+            },
+            newGame()
+            {
+                this.pokemon = null;
+                this.showPokemon = false;
+                this.showAnswer = false;
+                this.message = "";
+                this.pokemonArr = [];
+                this.mixPokemonArry();
+            }
+        },
+        mounted(){
+            //console.log('mounted');
+            this.mixPokemonArry();
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
